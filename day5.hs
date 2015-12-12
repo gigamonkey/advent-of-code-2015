b53d6b219a32163e97a58ed679b56636487018f8
Peter Seibel <peter@gigamonkeys.com>
2015-12-12 10:42:19 -0800

    Day 5, part 2

--- day5.hs
+++ day5.hs
@@ -15,4 +15,19 @@ noNaughty         = not . any (`elem` ["ab", "cd", "pq", "xy"]) . pairs
 
 nice = threeVowels &&& atLeastOneDoubled &&& noNaughty
 
-main = puzzle >>= print . length . (filter nice)
+-- part 2
+
+doubled [] = False
+doubled (x:[]) = False
+doubled (x:xs) = (x `elem` tail xs) || (doubled xs)
+
+doubledPair :: Eq a => [a] -> Bool
+doubledPair = doubled . pairs
+
+separated (x:[]) = False
+separated (x:y:[]) = False
+separated (x:y:xs) = x == head xs || separated (y:xs)
+
+nice' = doubledPair &&& separated
+
+main = puzzle >>= print . length . (filter nice')
