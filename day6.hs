3d3fa3c7016f3ce5e1afb84690a7e2e45218aacc
Peter Seibel <peter@gigamonkeys.com>
2015-12-12 20:35:17 -0800

    Day 6, part 2

--- day6.hs
+++ day6.hs
@@ -1,13 +1,14 @@
-import Control.Applicative
+import Control.Applicative ((<|>))
 import Control.Monad
-import Data.List (isPrefixOf)
+import Data.List (isPrefixOf, null)
+import qualified Data.Map.Strict as M
 import Data.Maybe
-import Data.Set (difference, intersection, union, fromList, Set, empty, size)
+import qualified Data.Set as S
 import System.IO
 
--- part 1 answer: 400410
+type Lights = S.Set (Int, Int)
 
-data Op = On (Set (Int, Int)) | Off (Set (Int, Int)) | Toggle (Set (Int, Int)) deriving (Show)
+data Op = On Lights | Off Lights | Toggle Lights deriving (Show)
 
 puzzle = liftM lines (openFile "puzzles/day6.puzzle" ReadMode >>= hGetContents)
 
@@ -20,24 +21,33 @@ parse x = fromMaybe (error "No parse") (tryParse On "turn on " x <|> tryParse Of
 
 tryParse f p x = if p `isPrefixOf` x then Just $ f $ parseRest $ drop (length p) x else Nothing
 
-parseRest x = fromList [(x, y) | x <- [x1..x2], y <- [y1..y2]] where
+parseRest x = S.fromList [(x, y) | x <- [x1..x2], y <- [y1..y2]] where
     [a, _, b] = splitOn ' ' x
     [x1, y1] = map read (splitOn ',' a)
     [x2, y2] = map read (splitOn ',' b)
 
-turnOn, turnOff, toggle :: Set (Int, Int) -> Set (Int, Int) -> Set (Int, Int)
+turnOn = S.union
 
-turnOn lights on = lights `union` on
-
-turnOff lights off = lights `difference` off
+turnOff = S.difference
 
 toggle lights toggle = turnOn (turnOff lights off) on where
-    off = lights `intersection` toggle
-    on  = toggle `difference` lights
+    off = lights `S.intersection` toggle
+    on  = toggle `S.difference` lights
 
-run = foldl foo Data.Set.empty where
-    foo l (On s)     = turnOn l s
-    foo l (Off s)    = turnOff l s
-    foo l (Toggle s) = toggle l s
+run = foldl eval S.empty where
+    eval l (On s)     = turnOn l s
+    eval l (Off s)    = turnOff l s
+    eval l (Toggle s) = toggle l s
 
-main = puzzle >>= print . (400410 ==) . size . run . map parse
+run' = foldl eval M.empty where
+    eval m (On s)     = foldl (\m' k -> M.insertWith (+) k 1 m') m (S.elems s)
+    eval m (Off s)    = foldl (\m' k -> M.insertWith dim k 0 m') m (S.elems s)
+    eval m (Toggle s) = foldl (\m' k -> M.insertWith (+) k 2 m') m (S.elems s)
+    dim :: Int -> Int -> Int
+    dim _ old = max 0 (old - 1)
+
+part1 = puzzle >>= print . (400410 ==) . S.size . run . map parse
+
+part2 = puzzle >>= print . (M.foldl (+) 0) . run' . map parse
+
+main = part2
