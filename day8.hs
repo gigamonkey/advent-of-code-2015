dff567ca68bf2c0af60173a921984e94e3b9280b
Peter Seibel <peter@gigamonkeys.com>
2015-12-21 12:34:50 -0800

    Day 8, part 2

--- day8.hs
+++ day8.hs
@@ -10,12 +10,23 @@ codeCharacters = T.length
 
 textCharacters = subtract 2 . stringCharacters . T.unpack
 
+textCharacters' = (+ 2) . reencodedCharacters . T.unpack
+
 -- this assumes all the strings are properly formatted, e.g. there are
 -- no stranded '\'s, etc.
-stringCharacters []                  = 0
-stringCharacters ('\\':'"':rest)     = 1 + stringCharacters rest
-stringCharacters ('\\':'\\':rest)    = 1 + stringCharacters rest
-stringCharacters ('\\':'x':_:_:rest) = 1 + stringCharacters rest
-stringCharacters (x:xs)              = 1 + stringCharacters xs
+stringCharacters []                = 0
+stringCharacters ('\\':'"':xs)     = 1 + stringCharacters xs
+stringCharacters ('\\':'\\':xs)    = 1 + stringCharacters xs
+stringCharacters ('\\':'x':_:_:xs) = 1 + stringCharacters xs
+stringCharacters (x:xs)            = 1 + stringCharacters xs
 
-main = view $ fold (fmap (\t -> codeCharacters t - textCharacters t) puzzle) F.sum
+reencodedCharacters []        = 0
+reencodedCharacters ('\\':xs) = 2 + reencodedCharacters xs
+reencodedCharacters ('"':xs)  = 2 + reencodedCharacters xs
+reencodedCharacters (x:xs)    = 1 + reencodedCharacters xs
+
+sum1 t = codeCharacters t - textCharacters t
+sum2 t = textCharacters' t - codeCharacters t
+
+
+main = view $ fold (fmap sum2 puzzle) F.sum
