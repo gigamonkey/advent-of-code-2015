d8ba9d9be9b99ac9ac41f3cabd544c77330c4930
Peter Seibel <peter@gigamonkeys.com>
2015-12-24 15:00:15 -0700

    Day 10, part 2

--- day10.hs
+++ day10.hs
@@ -1,16 +1,9 @@
-import Data.List (maximumBy, minimumBy, nub, permutations)
-import Data.Maybe (fromJust)
-import Data.Ord (comparing)
-import Turtle
-import qualified Control.Foldl as F
-import qualified Data.Map.Strict as M
-
 seed = "3113322113"
 
-lookandsay s = s : lookandsay (look 0 'a' s) where
-    look 0 _ (x:xs) = look 1 x xs
-    look n c [] = say n c
+-- 0th element is the original seed. Nth element is the result of N iterations.
+lookandsay s = s : lookandsay (look 1 (head s) (tail s)) where
+    look n c []     = say n c
     look n c (x:xs) = if x == c then look (n + 1) c xs else say n c ++ look 1 x xs
-    say n c = show n ++ [c]
+    say n c         = show n ++ [c]
 
-main = print $ length (lookandsay seed !! 40)
+main = print $ (length (lookandsay seed !! 40), length (lookandsay seed !! 50))
