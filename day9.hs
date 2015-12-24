7380687160035bddbebc9668946fec65c59892c8
Peter Seibel <peter@gigamonkeys.com>
2015-12-24 10:26:01 -0700

    Refactoring day 9

--- day9.hs
+++ day9.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.List
-import Data.Maybe
-import Data.Ord
+import Data.List (maximumBy, minimumBy, nub, permutations)
+import Data.Maybe (fromJust)
+import Data.Ord (comparing)
 import Turtle
 import qualified Control.Foldl as F
 import qualified Data.Map.Strict as M
@@ -11,24 +11,25 @@ data Problem = Problem [String] (M.Map (String, String) Int)
 
 puzzle = fold (input "puzzles/day9.puzzle") F.list
 
-parse p = Problem cities distances where
-    edges     = map (head . (match edge)) p
+problem p = Problem cities distances where
+    edges     = map (head . match edge) p
     cities    = nub [ city | ((a, b), _) <- edges, city <- [a, b] ]
     distances = foldl (\m (p, d) -> M.insert p d m) M.empty edges
-    city      = some letter
     edge      = do { a <- city; " to "; b <- city; " = "; d <- decimal; return (pair a b, d) }
+    city      = some letter
 
-pair a b = (min a b, max a b)
-
-pathLength ds path = foldl len 0 (pairs path) where
+distance ds path = foldl len 0 (pairs path) where
     pairs xs = zip xs (tail xs)
-    len d (a, b) = d + (fromJust $ M.lookup (pair a b) ds)
+    len d (a, b) = d + fromJust (M.lookup (pair a b) ds)
+
+pair a b = if a < b then (a, b) else (b, a)
 
 main = do
   p <- puzzle
-  let (Problem cities distances) = parse p
-  let len   = pathLength distances
-  let cmp   = comparing $ len
+  let (Problem cities distances) = problem p
+  let len   = distance distances
+  let cmp   = comparing len
   let paths = permutations cities
-  print $ len $ minimumBy cmp paths
-  print $ len $ maximumBy cmp paths
+  let max = len . maximumBy cmp
+  let min = len . minimumBy cmp
+  print $ (min paths, max paths)
