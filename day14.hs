c915298e6574d03af36f2b6b668859f6cbc9f97a
Peter Seibel <peter@gigamonkeys.com>
2015-12-25 14:15:28 -0700

    Day 14, part 2

--- day14.hs
+++ day14.hs
@@ -4,10 +4,13 @@ import Turtle
 import Data.List (nub, permutations, minimumBy, maximumBy)
 import Data.Ord (comparing)
 import qualified Control.Foldl as F
+import qualified Data.Map.Strict as M
 
 data Reindeer = Reindeer Text Int Int Int deriving (Show)
 
 puzzle = fold (input "puzzles/day14.puzzle") F.list
+
+duration = 2503
 
 fact = do
   name <- (star letter)
@@ -26,4 +29,16 @@ distance n (Reindeer name speed flyTime restTime) = (name, dist) where
     lastFlight = min flyTime left
     dist       = ((rounds * flyTime) + lastFlight) * speed
 
-main = puzzle >>= print . maximumBy (comparing snd) . map (distance 2503 . head . match fact)
+reindeer p = map (head . match fact) p
+
+race reindeer = [ map (distance s) reindeer | s <- [1..duration] ]
+
+leaders = map ahead where
+    ahead xs = map fst $ filter ((== m xs) . snd) xs
+    m xs = snd (maximumBy (comparing snd) xs)
+
+counts = foldl (\m x -> M.insertWith (+) x 1 m) M.empty
+
+score = maximum . M.elems . counts . concat
+
+main = puzzle >>= print . score . leaders . race . reindeer
