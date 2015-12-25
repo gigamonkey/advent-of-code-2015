1acca73b2f151e7483920fc24210addc2ff23160
Peter Seibel <peter@gigamonkeys.com>
2015-12-24 20:18:17 -0700

    Day 12, part 1

---
+++ day12.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad
+import Data.Aeson
+import Data.HashMap.Strict
+import Data.Maybe
+import qualified Data.ByteString.Lazy as B
+
+puzzle = B.readFile "puzzles/day12.puzzle"
+
+puzzleJson = liftM (fromJust . decode) puzzle
+
+total (Object o) = foldl' (\acc v -> acc + total v) 0 o
+total (Array  a) = foldl (\acc v -> acc + total v) 0 a
+total (Number n) = n
+total _ = 0
+
+main = puzzleJson >>= print . total
