6374705fe833a334a61a7fda34bdce391b4059a5
Peter Seibel <peter@gigamonkeys.com>
2015-12-26 14:16:51 -0700

    Day 19, part 2

--- day19.hs
+++ day19.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Turtle
 import Data.List
-import qualified Control.Foldl as F
 import Data.Text (unpack)
+import Data.Tuple (swap)
+import Turtle
+import qualified Control.Foldl as F
 
 replacements = map (head . match p) `fmap` fold (input "puzzles/day19.replacements") F.list where
     p = (,) <$> some letter <* " => " <*> some letter
@@ -14,7 +15,14 @@ allReplacements rs s = nub [ replace f t a b | (f, t) <- rs, (a, b) <- splits, i
     replace f t a b = a ++ t ++ drop (length f) b
     splits = zip (inits s) (tails s)
 
+generate rs (steps, molecule) = nextstep ++ (concatMap (generate rs) nextstep) where
+    nextstep = nub [ (steps + 1, next) | next <- allReplacements rs molecule ]
+
+search molecule = Data.List.find ((== molecule) . snd)
+
+flipped = map swap
+
 main = do
   rs <- replacements
   s <- start
-  print $ length $ allReplacements rs s
+  print $ search "e" $ generate (flipped rs) (0, s)
