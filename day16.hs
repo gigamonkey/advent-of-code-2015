e0c6044fddae42a84921d635569b4e3184555aae
Peter Seibel <peter@gigamonkeys.com>
2015-12-25 17:22:45 -0700

    Day 16, part 1

---
+++ day16.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Turtle
+import Data.Maybe (fromMaybe)
+import Data.List (nub, permutations, minimumBy, maximumBy)
+import Data.Ord (comparing)
+import qualified Control.Foldl as F
+import qualified Data.Map.Strict as M
+
+matching = [
+ ("children", 3),
+ ("cats", 7),
+ ("samoyeds", 2),
+ ("pomeranians", 3),
+ ("akitas", 0),
+ ("vizslas", 0),
+ ("goldfish", 5),
+ ("trees", 3),
+ ("cars", 2),
+ ("perfumes", 1)]
+
+data AuntSue = AuntSue { number :: Int, characteristics :: M.Map Text Int } deriving (Show)
+
+puzzle = fold (input "puzzles/day16.puzzle") F.list
+
+labels = [ "children", "cats", "samoyeds", "pomeranians", "akitas", "vizslas", "goldfish", "trees", "cars", "perfumes"]
+
+aunt = do
+  number <- "Sue " *> decimal <* ": "
+  characteristics <- choice (map labeled labels) `sepBy` ", "
+  return (AuntSue number (M.fromList characteristics))
+
+labeled label = do { l <- text label; ": "; n <- decimal; return (l, n) }
+
+ok (AuntSue n c) = all (\(l, n) -> fromMaybe True ((M.lookup l c) >>= (\x -> Just (x == n)))) matching
+
+main = puzzle >>= print . number . head . (filter ok) . map (head . match aunt)
