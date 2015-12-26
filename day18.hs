b09d22211620cd6a99384626ccf0ca18275d7951
Peter Seibel <peter@gigamonkeys.com>
2015-12-25 22:42:22 -0700

    A space.

--- day18.hs
+++ day18.hs
@@ -10,7 +10,7 @@ countOn = length . filter id
 
 neighbors m (x, y) = map (\p -> inBounds p && m ! p) [ (x + i, y + j) | (i, j) <- offsets ] where
     inBounds (x, y) = 0 < x && x <= nrows m && 0 < y && y <= ncols m
-    offsets = [ (x, y) | x <- [-1 .. 1], y <- [-1 .. 1], (x,y) /= (0, 0) ]
+    offsets = [ (x, y) | x <- [-1 .. 1], y <- [-1 .. 1], (x, y) /= (0, 0) ]
 
 nextCell m p = alive (m ! p) where
     alive x = count == 3 || (x && count == 2)
