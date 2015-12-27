95ee96b8e4dfcef2a25eb8e28e6f0f624d015d84
Peter Seibel <peter@gigamonkeys.com>
2015-12-27 09:13:26 -0700

    Turn simple day 20 into command line tool.

--- day20.hs
+++ day20.hs
@@ -1,16 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Turtle (options, argInt)
 import Control.Applicative
 import Data.List
 
-puzzle = 29000000
+presents = foldr combine zeros [ elf n | n <- [1 .. ] ] where
+    elf n = cycle $ n : take (n - 1) zeros
+    combine (x:xs) ys = x : zipWith (+) xs ys
+    zeros = repeat 0
 
-zeros = repeat 0
-
-elf n = cycle $ n : take (n - 1) zeros
-
-combine (x:xs) ys = x : zipWith (+) xs ys
-
-presents = foldr combine zeros [ elf n | n <- [1 .. ] ]
-
-lim = puzzle `div` 10
-
-main = print $ find ((>= lim) . snd) (zip [1..] presents)
+main = do
+  n <- options "Foo" (argInt  "min" "Minimum number of presents")
+  print $ find ((>= n) . snd) (zip [1..] presents)
