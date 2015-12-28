ca91bed067f07a4144bfbf709d0646dbd353d8c0
Peter Seibel <peter@gigamonkeys.com>
2015-12-28 14:03:44 -0800

    Tidying

--- day20y.hs
+++ day20y.hs
@@ -1,13 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Applicative
-import Data.List
-import Data.Numbers.Primes
-import Turtle (options, argInt)
-import qualified Data.Map.Strict as M
-import qualified Data.Vector as V
-
-type Tree = M.Map Int [Int]
+import Control.Applicative ((<$>), (<*>))
+import Data.List (nub, sort)
+import Data.Numbers.Primes (primes)
 
 answer = 665280
 
@@ -17,27 +12,27 @@ takeInclusive p = (\(a, b) -> a ++ [ head b ]) . span p
 
 divisors p xs = sort $ nub $ (*) <$> xs <*> [1, p]
 
-powers p = takeInclusive ((<= puzzle) . sum . snd) . iterate step where
+takeBelow = takeInclusive $ (<= puzzle) . sum . snd
+
+powers p = takeBelow . iterate step where
     step = (,) <$> (p *) . fst <*> (divisors p) . snd
 
-start = M.fromList $ powers 2 (1, [1])
+start = powers 2 (1, [1])
 
-addPower m p = M.fromList $ takeInclusive ((<= puzzle) . sum . snd) $ sort next where
-    next    = [ x | c <- sort $ M.assocs m, x <- takeWhile ((<= upper) . fst) $ powers p c ]
-    upper   = maximum $ M.keys m
+addPower m p = takeBelow $ nub $ sort next where
+    next  = [ x | c <- sort m, x <- takeWhile ((<= upper) . fst) $ powers p c ]
+    upper = maximum $ map fst m
 
 addPowers = foldl addPower start
 
+addPowers' m [] = m
 addPowers' m (p:ps) = if m == next then m else addPowers' next ps where
     next = addPower m p
 
-findFirst m = fst $ head $ filter ((>= puzzle) . sum . snd) $ sort $ M.assocs m
+findFirst m = fst $ head $ filter ((>= puzzle) . sum . snd) $ sort m
 
-bar m (p:ps) = ((p, maximum $ M.keys next, M.size next - M.size m), next) : bar next ps where
+bar m (p:ps) = ((p, maximum $ map fst next, length next - length m), next) : bar next ps where
     next = addPower m p
-
-foo m p = (length [ x | c <- sort $ M.assocs m, x <- takeWhile ((<= upper) . fst) $ powers p c ])  - (length $ M.elems m) where
-    upper = maximum $ M.keys m
 
 main = do
   print $ addPowers (take 10 primes)
