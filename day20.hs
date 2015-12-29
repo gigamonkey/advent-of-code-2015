6a55aca9a988076d511dd23a8284f46d6abff46e
Peter Seibel <peter@gigamonkeys.com>
2015-12-29 15:03:28 -0800

    Day 20, part 1.

--- day20.hs
+++ day20.hs
@@ -1,16 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
+import Data.List
+import Data.Numbers.Primes
 
-import Data.List (find)
-import Data.Maybe (fromJust)
-import Turtle (options, argInt)
+presents = sum . map product . nub . tail . subsequences . primeFactors
 
-presents :: [Int]
-presents = foldr combine zeros [ elf n | n <- [1 .. ] ] where
-    elf n = cycle $ n : take (n - 1) zeros
-    combine (x:xs) ys = x : zipWith (+) xs ys
-    zeros = repeat 0
-
-main :: IO ()
-main = do
-  n <- options "Advent of Code, day 20 solver" (argInt  "min" "Minimum number of presents")
-  print $ fst $ fromJust $ find ((>= n) . snd) (zip [1..] presents)
+main = print $ find ((>= 2900000) . presents) [1 ..]
