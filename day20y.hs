3d5ceb6735b266a2044af8fcd41d60fa847bbdc6
Peter Seibel <peter@gigamonkeys.com>
2015-12-29 09:38:14 -0800

    Add types.

--- day20y.hs
+++ day20y.hs
@@ -3,37 +3,50 @@
 import Control.Applicative ((<$>), (<*>))
 import Data.List (nub, sort)
 import Data.Numbers.Primes (primes)
+import Turtle (options, argInt)
 
-answer = 665280
+type Leaf = (Int, [Int])
+type Tree = [Leaf]
 
+theAnswer :: Int
+theAnswer = 665280
+
+puzzle :: Int
 puzzle = 2900000
 
-takeInclusive p = (\(a, b) -> a ++ [ head b ]) . span p
+takeInclusive :: (a -> Bool) -> [a] -> [a]
+takeInclusive p = (\(a, b) -> a ++ take 1 b) . span p
 
+divisors :: Int -> [Int] -> [Int]
 divisors p xs = sort $ nub $ (*) <$> xs <*> [1, p]
 
-takeBelow = takeInclusive $ (<= puzzle) . sum . snd
+takeBelow :: Int -> Tree -> Tree
+takeBelow lim = takeInclusive ((<= lim) . sum . snd)
 
-powers p = takeBelow . iterate step where
+powers :: Int -> Int -> Leaf -> Tree
+powers lim p = takeBelow lim . iterate step where
     step = (,) <$> (p *) . fst <*> (divisors p) . snd
 
-start = powers 2 (1, [1])
+start :: Int -> Tree
+start lim = powers lim 2 (1, [1])
 
-addPower m p = takeBelow $ nub $ sort next where
-    next  = [ x | c <- sort m, x <- takeWhile ((<= upper) . fst) $ powers p c ]
+addPower :: Int -> Tree -> Int -> Tree
+addPower lim m p = takeBelow lim $ nub $ sort next where
+    next  = [ x | c <- sort m, x <- takeWhile ((<= upper) . fst) $ powers lim p c ]
     upper = maximum $ map fst m
 
-addPowers = foldl addPower start
+addPowers' _ m [] = m
+addPowers' lim m (p:ps) = if m == next then m else addPowers' lim next ps where
+    next = addPower lim m p
 
-addPowers' m [] = m
-addPowers' m (p:ps) = if m == next then m else addPowers' next ps where
-    next = addPower m p
+answer :: Tree -> Int
+answer = maximum . map fst
 
-findFirst m = fst $ head $ filter ((>= puzzle) . sum . snd) $ sort m
-
-bar m (p:ps) = ((p, maximum $ map fst next, length next - length m), next) : bar next ps where
-    next = addPower m p
+--bar :: Int -> Tree -> [Int] -> [((Int, Int, Int), Tree)]
+bar lim =  map fst $ takeWhile (\((_, _, _, d), _) -> d > 0) $ foo (start lim) (drop 1 primes) where
+    foo m (p:ps) = ((p, last $ map fst next, length $ snd $ last next, length next - length m), next) : foo next ps where
+                                                     next = addPower lim m p
 
 main = do
-  print $ addPowers (take 10 primes)
-  print $ addPowers' start (take 10 primes)
+  lim <- options "Advent of Code, Day 20 solver" (argInt  "min" "Minimum number of presents")
+  print $ answer $ addPowers' lim (start lim) (drop 1 primes)
