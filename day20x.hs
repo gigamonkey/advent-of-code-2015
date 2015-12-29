b0c63db3e0ca8a886a37dca9918143a39cb2a1b8
Peter Seibel <peter@gigamonkeys.com>
2015-12-29 15:00:07 -0800

    The end of my day 20 failing.

--- day20x.hs
+++ day20x.hs
@@ -3,6 +3,7 @@
 import Control.Applicative
 import Data.List
 import Data.Maybe
+import Data.Numbers.Primes
 import Data.Ord
 import Turtle
 import Turtle (options, argInt)
@@ -16,88 +17,66 @@ puzzle = 29000000
 
 --lim = puzzle `div` 10
 
-bestish, better, evenbetter :: Factors
+bestish, better, evenbetter, best :: Factors
 bestish    = [(2,5),(3,1),(5,1),(7,1),(241,1)]
 better     = [(2,3),(3,3),(5,1),(7,1),(107,1)]
 evenbetter = [(2,2),(3,5),(5,1),(7,1),(23,1)]
-
-allPrimes :: [Int]
-allPrimes = 2 : filter isPrime [3 .. ] where
-    isPrime n = not (any ((== 0) . (n `mod`)) $ takeWhile ((<= n) . (^ 2)) allPrimes)
+best       = [(2,6),(3,3),(5,1),(7,1),(11,1)]
 
 start :: Int -> Factors
 start lim = [(2,floor $ logBase 2 (fromIntegral lim))]
 
--- Number of presents delivered to house with the given prime factorization.
+factors :: Int -> Factors
+factors = map (\x -> (head x, length x)) . group . primeFactors
+
+number :: Factors -> Int
+number = product . map (\(b, e) -> b ^ e)
+
+divide :: Factors -> Factors -> Factors
+divide xs ys = filter ((/= 0) . snd) $ M.toList $ foldl step (M.fromList xs) ys where
+    step m (b, e) = M.insertWith (+) b (- e) m
+
+mult :: Factors -> Factors -> Factors
+mult xs ys = M.toList $ foldl step (M.fromList xs) ys where
+    step m (f, n) = M.insertWith (+) f n m
+
+divisors :: Factors -> [Factors]
+divisors = foldl step [] . map powers where
+    step a b = sortBy (comparing number) $ nub (a ++ b ++ ((++) <$> a <*> b))
+    powers (n, c) = [ [(n, i)] | i <- [1 .. c] ]
+
 presents :: Factors -> Int
-presents = (+ 1) . sum . map house . divisors
+presents = (+ 1) . sum . map number . divisors
 
--- House number for given prime factorization.
-house :: Factors -> Int
-house = product . map (\(b, e) -> b ^ e)
-
--- Does the given house get enough presents?
 good :: Int -> Factors -> Bool
 good lim = (>= lim) . presents
 
--- All the divisors for the given prime factorization
-divisors :: Factors -> [Factors]
-divisors = foldl step [] . map powers where
-    step a b = sortBy (comparing house) $ nub (a ++ b ++ ((++) <$> a <*> b))
-    powers (n, c) = [ [(n, i)] | i <- [1 .. c] ]
+
+up lim n = Data.List.find (good lim) nexts where
+    p = fst $ last n
+    nexts = [ (n `divide` [(p, 1)]) `mult` (factors x) | x <- [1 .. p] ]
 
 fixpoint f x = if f x == x then x else fixpoint f (f x)
 
 search lim = fixpoint step (start lim) where
-    primes = tail (takeWhile (<= lim) allPrimes)
-    step xs = fixpoint (down lim primes) (fixpoint (across lim primes)  xs)
+    ps = tail (takeWhile (<= lim) primes)
+    step xs = fixpoint (down lim ps) (fixpoint (across lim ps)  xs)
 
-across lim primes fs = step fs primes where
-    step xs (p:ps) = fromMaybe xs ((try' lim p xs) >>= (\xs' -> return (step xs' ps)))
+across lim ps fs = step fs ps where
+    step xs (p:ps) = fromMaybe xs ((try lim p xs) >>= (\xs' -> return (step xs' ps)))
 
-down lim primes fs = step fs primes where
-    step xs (p:ps) = fromMaybe (step xs ps) (try' lim p xs)
+down lim ps fs = step fs ps where
+    step xs (p:ps) = fromMaybe (step xs ps) (try lim p xs)
 
---foldem :: Factors -> Int -> Factors
---foldem xs n = if next /= xs && good next then foldem next n else xs
---    where next = try n xs
+shrink' lim p n = Data.List.find (good lim) nexts where
+    bigger = filter ((> number p) . number) $ divisors n
+    nexts = [ (n `divide` d) `mult` p | d <- bigger ]
 
-remove :: Factors -> Factors -> Factors
-remove xs ys = filter ((/= 0) . snd) $ M.toList $ foldl step (M.fromList xs) ys where
-    step m (b, e) = M.insertWith (+) b (- e) m
-
-add :: Factors -> Int -> Factors
-add xs f = M.toList $ M.insertWith (+) f 1 (M.fromList xs)
-
-foo :: Int -> Factors -> (Factors, Int, Int, Bool)
-foo lim xs = (xs, house xs, presents xs, good lim xs)
-
-try :: Int -> Factors -> Factors
-try p xs = next where
-    next = if not (null ds) then add (remove xs (head ds)) p else xs
-    ds = dropWhile ((<= p) . house) $ divisors xs
-
-try' lim p xs = if good lim next then (Just next) else Nothing where
-    next = try p xs
-
-xdivisors :: Factors -> [Int]
-xdivisors = (1 :) . foldl step [] . map powers where
-    step a b = sort (nub (a ++ b ++ ((*) <$> a <*> b)))
-    powers (n, c) = [ n ^ i | i <- [1 .. c] ]
-
-factors :: Int -> M.Map Int Int
-factors n = snd (foldl step (n, M.empty) (takeWhile (<= n) allPrimes)) where
-    step (x, fs) p = if x `mod` p == 0 then step ((x `div` p), (M.insertWith (+) p 1 fs)) p else (x, fs)
-
-(.>) a n = a >>= try' n
-
---ok :: Int -> Factors -> Bool
---ok lim xs = foo lim xs where
---    foo [] _ = False
---    foo (x:xs) n = n >= lim || foo xs (n + x)
-
+try lim p n = if good lim next then (Just next) else Nothing where
+    next = if not (null d) then (n `divide` (head d)) `mult` [(p, 1)] else n
+    d = dropWhile ((<= p) . number) $ divisors n
 
 main :: IO ()
 main = do
   n <- options "Foo" (argInt  "min" "Minimum number of presents")
-  print $ house (search n)
+  print $ number (search n)
