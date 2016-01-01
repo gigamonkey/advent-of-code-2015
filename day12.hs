f8dc79dd9ee46a1e7cdb68dad10d73a77932367c
Peter Seibel <peter@gigamonkeys.com>
2015-12-31 17:09:57 -0800

    Hand applying changes suggested in PR from michaelt

--- day12.hs
+++ day12.hs
@@ -2,19 +2,20 @@
 
 import Control.Monad
 import Data.Aeson
-import Data.HashMap.Strict
 import Data.Maybe
 import qualified Data.ByteString.Lazy as B
+import qualified Data.HashMap.Strict as M
+import qualified Data.Vector as V
 
 puzzle = B.readFile "puzzles/day12.puzzle"
 
 puzzleJson = liftM (fromJust . decode) puzzle
 
-total (Object o) = if hasRed o then 0 else foldl' (\acc v -> acc + total v) 0 o
-total (Array  a) = foldl (\acc v -> acc + total v) 0 a
+total (Object o) = if hasRed o then 0 else sum $ M.map total o
+total (Array  a) = sum $ V.map total a
 total (Number n) = n
 total _ = 0
 
-hasRed = elem "red" . elems
+hasRed = elem "red" . M.elems
 
 main = puzzleJson >>= print . total
